cfae3b67a827aff7761a756b2b25bd2efcfd4cbe
EduardTeixeira <eduard.teixeira@adsrock.com.br>
2020-03-03 22:33:53 -0300

    inserindo rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { CourseService } from './courses/course.service';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { Error404Component } from './error-404/error-404.component';
 
 @NgModule({
   declarations: [
@@ -14,10 +17,25 @@ import { ReplacePipe } from './pipe/replace.pipe';
     CourseListComponent,
     StarComponent,
     ReplacePipe,
+    NavBarComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: '', redirectTo: 'courses', pathMatch: 'full'
+        },
+        {
+          path: 'courses', component: CourseListComponent,
+        },
+        {
+          path: '**', component: Error404Component,
+        }
+      ]
+    ),
   ],
   providers: [CourseService],
   bootstrap: [AppComponent]
